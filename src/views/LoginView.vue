3c2913b9dba10e11f65e22d8f0c17e727b274045
Tomás Danko <tdanko@fi.uba.ar>
2025-05-14 00:12:59 -0300

    aesthetic changes

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,8 +2,16 @@
   <v-container fluid class="d-flex align-center justify-center fill-height">
     <v-row justify="center" align="center">
       <v-col cols="12" sm="8" md="4">
-        <v-card class="pa-4" elevation="10">
-          <v-card-title class="text-h5 text-center">Iniciar Sesión</v-card-title>
+        <v-card class="pa-10 justify-center" color="" elevation="10">
+          <div class="d-flex justify-center">
+            <v-img
+            max-width="400"
+
+            aspect-ratio="4/3"
+            contain
+            src="/login-icon.png"
+          ></v-img>
+          </div>
 
             <v-card-text>
               <v-form v-model="valid">
@@ -29,14 +37,16 @@
             </v-card-text>
   
             <v-card-actions class="justify-center">
-              <v-btn color="primary" :disabled="!valid" @click="handleLogin">
+              <v-btn variant="elevated" color="secondary" :disabled="!valid" @click="handleLogin">
                 Ingresar
               </v-btn>
             </v-card-actions>
 
-            <v-card-text class="text-center">
-            <router-link to="/register">¿No tenés cuenta? Registrate</router-link>
-            </v-card-text>
+            <v-card-actions class="justify-center">
+              <v-btn variant="elevated" color="primary" @click="handleRegister">
+                Crear una cuenta
+              </v-btn>
+            </v-card-actions>
 
           </v-card>
         </v-col>
@@ -46,44 +56,6 @@
   
 <script >
   import { ref } from 'vue'
-  //import axios from 'axios'
-  import router from '@/router'
-  /*const email = ref('')
-  const password = ref('')
-  const showPassword = ref(false)
-  const valid = ref(false)
-  
-  const emailRules = [
-    v => !!v || 'Email es requerido',
-    v => /.+@.+\..+/.test(v) || 'Email no válido',
-  ]
-  
-  const passwordRules = [
-    v => !!v || 'Contraseña es requerida',
-    v => v.length >= 6 || 'Mínimo 6 caracteres',
-  ]*/
-
-
-
-
-    /*const response = axios.post('http://localhost:3000/api/login', {
-      email: email.value,
-      password: password.value,
-    })
-
-    // cambiarlo por un token cuando haya
-    const userData = response.data
-    localStorage.setItem('user', JSON.stringify(userData))
-    console.log('Login exitoso:', userData)
-
-
-    // redirigir a home
-    router.push('/')
-    */
-  /*
-  } catch (error) {
-    console.error('Error al loguear:', error.response?.data || error.message)
-  }*/
 
   export default {
   name: 'LoginView',
@@ -112,7 +84,7 @@
   },
   created () {
     if (this.loggedIn) {
-      router.push('/')
+      this.$router.push('/')
     }
   },
   methods: {
@@ -127,7 +99,6 @@
       this.$store.dispatch('login', user).then(
         () => {
           console.log('Login exitoso')
-          //router.push('/')
           this.$router.push('/')
           console.log(this.$router)
         },
@@ -135,6 +106,9 @@
           this.error = error.response ? error.response.data.message : 'El usuario es incorrecto.'
         }
       )
+    },
+    handleRegister () {
+      this.$router.push('/register')
     }
   },
 }
