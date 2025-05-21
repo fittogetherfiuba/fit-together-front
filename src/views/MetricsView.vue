59e55c4245798265c43d4c6d89ba9cf607725dee
juancpuch <jrobledo@fi.uba.ar>
2025-05-21 17:39:23 -0300

    card de metricas de actividades realizadas en la semana

--- src/views/MetricsView.vue
+++ src/views/MetricsView.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="pa-4 d-flex flex-column align-center">
     <WaterMetricsCard class="mb-4" />
-    <CaloriesMetricsCard />
+    <CaloriesMetricsCard class="mb-4" />
+    <ActivitiesMetricsCard />
   </div>
 </template>
 
 <script>
-import WaterMetricsCard    from '../components/WaterMetricsCard.vue'
-import CaloriesMetricsCard from '../components/CaloriesMetricsCard.vue'
+import WaterMetricsCard      from '../components/WaterMetricsCard.vue'
+import CaloriesMetricsCard   from '../components/CaloriesMetricsCard.vue'
+import ActivitiesMetricsCard from '../components/ExercisesMetricsCard.vue'
 
 export default {
   name: 'MetricsView',
   components: {
     WaterMetricsCard,
-    CaloriesMetricsCard
+    CaloriesMetricsCard,
+    ActivitiesMetricsCard
   }
 }
 </script>
