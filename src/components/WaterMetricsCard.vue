5c8b549615ac457b864b49e1b8178fe6f1167c15
juancpuch <jrobledo@fi.uba.ar>
2025-05-27 14:36:04 -0300

    arreglos en WaterMetricsCard por el merge de metrics

--- src/components/WaterMetricsCard.vue
+++ src/components/WaterMetricsCard.vue
@@ -1,15 +1,4 @@
 <template>
-    <v-card
-        class="pb-4 mt-4 mx-auto"
-        elevation="10"
-        max-width="500"
-    >
-        <!-- Título con fondo verde difuminado -->
-        <v-card-title class="text-h5 mb-4 text-center font-weight-bold bg-secondary">
-        <v-icon start icon="mdi-water"></v-icon>
-        Agua consumida esta semana
-        </v-card-title>
-=======
   <v-card
     class="mx-auto my-4"
     elevation="10"
@@ -24,19 +13,12 @@
         <Bar :data="chartData" :options="chartOptions"/>
       </div>
       <div v-else class="text-center py-4">Cargando gráfico...</div>
->>>>>>> metrics
 
-        <!-- Área de texto con fondo verde oscuro y fuente negra -->
-        <v-card-text
-            class="d-flex align-baseline justify-center text-h4 font-weight-bold bg-secondary-darken-2 text-black"
-            style="padding: 16px;"
-        >
-            <span>{{ totalLiters }}</span>
-            <span class="text-h4 font-weight-bold text-light-green--text">
-                &nbsp;L
-            </span>
-        </v-card-text>
-    </v-card>
+      <div class="text-center mt-4">
+        Total esta semana: {{ totalLiters.toFixed(2) }} L
+      </div>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
