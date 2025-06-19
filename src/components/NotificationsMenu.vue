5474ed782f9628755eb690175a98a60b1cf9f5b1
lorenzoahumada <lorenzosahumada@gmail.com>
2025-06-19 16:15:04 -0300

    goal notifications finished

--- src/components/NotificationsMenu.vue
+++ src/components/NotificationsMenu.vue
@@ -21,7 +21,7 @@
                 <td class="d-flex flex-row justify-center align-center">
                     <div class="font-weight-bold mr-2">{{ notification.message }}</div>
                     <div class="text-caption text-grey">
-                      {{ new Date(notification.timestamp).toLocaleTimeString() }}
+                      {{ new Date(notification.createdAt).toLocaleTimeString() }}
                     </div>
                 </td>
               </tr>
@@ -37,43 +37,60 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import { watch } from 'vue'
+import axios from 'axios'
+import eventBus from '../eventBus';
 
 export default {
   name: 'NotificationsMenu',
-  computed: {
-    ...mapGetters('notifications', ['notifications'])
-  },
   data() {
     return {
+      notifications: [],
+      oldNotifications: 0,
       menuOpen: false,
-      hasNewNotifications: false
+      hasNewNotifications: false,
     }
   },
   methods: {
-    clearAll() {
-      this.$store.dispatch('notifications/clearNotifications');
+    async clearAll() {
+      try {
+        await axios.delete('http://localhost:3000/api/notifications/delete_all/' + this.$store.state.main.user.userId.toString());
+        this.notifications = [];
+        localStorage.setItem('lastSeenNotifications', '0');
+      } catch (error) {
+        console.error('Error deleting notifications:', error);
+      }
+    },
+    async fetchNotifications(){
+      try {
+        const response = await axios.get('http://localhost:3000/api/notifications?userId=' + this.$store.state.main.user.userId.toString());
+        this.notifications = response.data.notifications;
+
+        const lastSeenCount = parseInt(localStorage.getItem('lastSeenNotifications')) || 0;
+
+        if (this.notifications.length > lastSeenCount) {
+          this.hasNewNotifications = true;
+        }
+        else {
+          this.hasNewNotifications = false;
+        }
+
+      } catch (error) {
+        console.error('Error fetching notifications:', error);
+      }
     }
   },
   watch: {
     menuOpen(val) {
       if (val) {
         this.hasNewNotifications = false
+        localStorage.setItem('lastSeenNotifications', this.notifications.length.toString());
       }
     }
   },
-  mounted() {
-    // Watch manual del getter computado
-    watch(
-      () => this.notifications,
-      (newList) => {
-        if (newList.length > 0) {
-          this.hasNewNotifications = true
-        }
-      },
-      { immediate: true, deep: true }
-    )
+  
+  async created() {
+    await this.fetchNotifications();
+    eventBus.on('new-notification', this.fetchNotifications);
   }
 }
 
